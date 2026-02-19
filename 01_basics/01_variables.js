5068014ca5644cf1df4adeb65f302cd17b4a947c
hetvii28 <hetvitshah28@gmail.com>
2026-02-19 18:11:13 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123456
+let accountEmail = "hetvitshah28@gmail.com"
+var accountPwd = "280905"
+accountCity = "Surat"
+let accountState;
+
+// accountId = 2 NOT ALLOWED
+
+
+accountEmail ="hey@gmail.com"
+accountPwd = "123456"
+accountCity = "Diu"
+
+console.log(accountId);
+
+/* PREFER NOT TO USE VAR BECAUSE OF 
+ISSUE IN BLOCK SCOPE & FUNCTIONAL SCOPE
+*/
+
+
+
+console.table([accountId, accountEmail,accountPwd,accountCity,accountState]);
