164fe2fd59a89e55d66167d668df6038dd115dea
hetvii28 <hetvitshah28@gmail.com>
2026-02-19 18:11:43 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+ "use strict";  //TREAT ALL JS CODE AS NEWER VERSION//
+
+//  alert(3+3) WE ARE USING node js, not BROWSER//
+
+console.log(3+3)
+
+let name="hetvi"
+let age=20
+let isLoggedIn=false
+
+//PRIMITIVE DATA TYPES :
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =? true/false
+//null => standalone value (representation of empty value)
+//undefined => value is not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
