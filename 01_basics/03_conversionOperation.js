69742aa26f2d8bb6bfdf1ce25d08ed2c95faa21e
hetvii28 <hetvitshah28@gmail.com>
2026-02-19 18:36:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "hetvi"
+
+// // console.log(typeof score);
+// // console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc => NaN
+//true => 1; false => 0
+//"hetvi" (STRING) => NaN 
+
+let isLoggedIn = "hetvi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"hetvi" => true
+
+let Number=33
+let strNum = String(Number)
+console.log(strNum);
+console.log(typeof strNum);
