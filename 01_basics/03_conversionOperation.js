2fe95a89275ae6b45468f93fe0ca509423ba297c
hetvii28 <hetvitshah28@gmail.com>
2026-02-20 19:38:29 +0000

    conversion/comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,39 @@ let Number=33
 let strNum = String(Number)
 console.log(strNum);
 console.log(typeof strNum);
+
+
+//**** OPERATIONS ****//
+
+let value =3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "heyy "
+let str2 = "how are you??"
+let str3 = str1 + str2
+// console.log(str3)
+// // console.log(str1+str2)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// // console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
