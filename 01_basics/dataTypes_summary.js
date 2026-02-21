9b4a43aec1e8cdeb394bb48b752cc3e52454b309
hetvii28 <hetvitshah28@gmail.com>
2026-02-21 09:14:34 +0000

    dataType_summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,35 @@
+ //PRIMITIVE :
+
+ // 7 TYPES : STRING, NUMBER, BOOLEAN, NULL,
+ //  UNDEFINED, SYMBOL,BIGINT
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol('123')
+ const id2 = Symbol('123')
+
+ console.log(id == id2)
+
+ const bigNum = 1234567890n
+
+
+
+ //Reference (NON - PRIMITIVE)
+ // ARRAY, OBJECTS, FUNCTIONS
+
+ const heros = ["ganesh","krishna","shiva"]
+ let myObj = {
+    name:"hetvi",
+    age:20,
+ }
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof id)
